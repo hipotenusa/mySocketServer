cf4d1c6f894b1ca2af762a85ba990fa305ff97cd
hipotenusa <fonso.gs@gmail.com>
2017-12-17 22:00:32 +0100

    Update express.js

--- express.js
+++ express.js
@@ -7,7 +7,8 @@ app.get('/', function (req, res) {
    res.send('Hello World');
 })
 
-var server = app.listen(8081, function () {
+var port = process.env.PORT || 5000;
+var server = app.listen(port, function () {
    var host = server.address().address
    var port = server.address().port
 
